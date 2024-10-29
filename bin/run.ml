809eab343755564f6938b917f7d75855dfc9f227
BardinPetr <bardin.petr@gmail.com>
2024-10-29 21:13:44 +0300

    Tests

--- bin/run.ml
+++ bin/run.ml
@@ -1,47 +1,16 @@
-(** [make_interpolation_runner name make_interpolator steps src_data] is a
-    function that wraps running interpolation, applying interpolated function to
-    test range and building output table. Function returns result string.
-    [make_interpolator] is a function that returns f(x) interpolation function
-    by input points *)
-let make_interpolation_runner name make_interpolator step src_data =
-  let min_x, _ = List.hd src_data in
-  let max_x, _ = List.nth src_data (List.length src_data - 1) in
-  let interpolated = make_interpolator src_data in
-  let res_data =
-    Datagen.gen_range min_x max_x step |> Datagen.apply_on_range interpolated
-  in
-  (name, interpolated, src_data, res_data)
-
-(** [apply_windowed_interpolations step methods input] is main function that
-    applies all selected interpolation methods over input *)
-let apply_windowed_interpolations step methods input =
-  let min_window_size =
-    List.fold_left (fun acc (_, _, win) -> min win acc) max_int methods
-  in
-  methods
-  |> List.map (fun (_, f, sz) ->
-         (* for each function make independent steram of sliding windows*)
-         Sequtils.sliding_window sz input
-         (* apply function over all windows *)
-         |> Seq.map (fun data -> Some (f step data))
-         (* add None-window-result paddings
-            so there won't be sync problems when doing zip *)
-         |> Seq.append
-              (Seq.repeat None |> Seq.take (max 0 (sz - min_window_size))))
-
 let main step method_names =
   let function_defs =
     [
       ( "linear",
-        make_interpolation_runner "Linear interpolation"
+        Solution.make_interpolation_runner "Linear interpolation"
           Interp.linear_interpolate,
         2 );
       ( "lagrange3",
-        make_interpolation_runner "Lagrange interpolation (3pt)"
+        Solution.make_interpolation_runner "Lagrange interpolation (3pt)"
           Interp.lagrange_interpolate,
         3 );
       ( "lagrange4",
-        make_interpolation_runner "Lagrange interpolation (4pt)"
+        Solution.make_interpolation_runner "Lagrange interpolation (4pt)"
           Interp.lagrange_interpolate,
         4 );
     ]
@@ -50,12 +19,6 @@ let main step method_names =
     function_defs |> List.filter (fun (tag, _, _) -> List.mem tag method_names)
   in
   Input.main_input_stream
-  |> Seq.memoize
-  |> apply_windowed_interpolations step methods
-  (* synchronized seq could now be merged now and flattened in round-robin pattern *)
-  |> Sequtils.nzip
-  |> Seq.flat_map List.to_seq
-  (* sort out only existing results *)
-  |> Seq.filter_map Fun.id
+  |> Solution.runner step methods
   |> Seq.map (fun (name, _, _, pts) -> Output.print_table name pts)
   |> Seq.iter print_string
--- test/runner.ml
+++ test/runner.ml
@@ -1,2 +1,69 @@
+open Alcotest
+
+let test_in =
+  List.to_seq
+    [ (0.0, 0.0); (1.571, 1.0); (3.142, 0.0); (4.712, -1.0); (12.568, 0.0) ]
+
+let test_out_1 =
+  [
+    [ 0.000; 0.637; 1.273 ];
+    [ 1.000; 0.363; -0.273 ];
+    [ 0.000; -0.637; -1.274 ];
+    [ -1.000; -0.873; -0.745; -0.618; -0.491; -0.364; -0.236; -0.109; 0.018 ];
+  ]
+
+let test_out_2 =
+  [
+    [ 0.000; 0.973; 0.841; 0.120; -0.674; -1.026 ];
+    [
+      1.000;
+      0.373;
+      -0.280;
+      -0.915;
+      -1.486;
+      -1.950;
+      -2.262;
+      -2.378;
+      -2.254;
+      -1.845;
+      -1.107;
+      0.004;
+    ];
+  ]
+
+let method_1 =
+  ( "linear",
+    Solution.make_interpolation_runner "Linear interpolation"
+      Interp.linear_interpolate,
+    2 )
+
+let method_2 =
+  ( "lagrange4",
+    Solution.make_interpolation_runner "Lagrange interpolation (4pt)"
+      Interp.lagrange_interpolate,
+    4 )
+
+let test_runner sel_method =
+  test_in
+  |> Solution.runner 1.0 [ sel_method ]
+  |> Seq.map (fun (_, _, _, seq_pts) ->
+         seq_pts |> Seq.map (fun (_, y) -> y) |> List.of_seq)
+  |> List.of_seq
+
+let gen_main_test test_method test_out () =
+  let testable = list (list (float 0.001)) in
+  let out = test_runner test_method in
+  check testable "Output" test_out out
+
 let () =
-  Utils.run_with_save_report "Lab3" "Lab3" (Sys.getenv_opt "REPORT_PATH") []
+  Utils.run_with_save_report "Lab3" "Lab3"
+    (Sys.getenv_opt "REPORT_PATH")
+    [
+      ( "Golden",
+        [
+          test_case "Linear golden test" `Quick
+            (gen_main_test method_1 test_out_1);
+          test_case "Lagrange golden test" `Quick
+            (gen_main_test method_2 test_out_2);
+        ] );
+    ]
